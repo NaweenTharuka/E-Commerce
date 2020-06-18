5a2446e01a5047da0c200c8d2e4c53a94611344b
NaweenTharuka <61498921+NaweenTharuka@users.noreply.github.com>
2020-06-18 18:54:24 +0530

    login interface

--- app/src/main/java/com/example/foodex/MainActivity.java
+++ app/src/main/java/com/example/foodex/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.foodex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
 
         joinNowButton = (Button)findViewById(R.id.main_join_now_btn);
         loginButton = (Button)findViewById(R.id.main_login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
