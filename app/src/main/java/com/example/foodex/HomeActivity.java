ce91320a8ce16cc92b7529a5174958aa075e471e
NaweenTharuka <61498921+NaweenTharuka@users.noreply.github.com>
2020-06-19 16:12:48 +0530

    login

---
+++ app/src/main/java/com/example/foodex/HomeActivity.java
@@ -0,0 +1,28 @@
+package com.example.foodex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+    private Button LogoutBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        LogoutBtn = (Button) findViewById(R.id.logout_btn);
+
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/foodex/Prevalent/Prevalent.java
@@ -0,0 +1,7 @@
+package com.example.foodex.Prevalent;
+
+import com.example.foodex.Model.Users;
+
+public class Prevalent {
+    private static Users currentOnlineUser;
+}
